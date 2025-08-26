41f530a21eef9a15c047b1bb95c06a41dbfe3441
TsunJapan <t.komura1989@gmail.com>
2025-08-26 20:13:18 +0900

    ・CodeRabbit日本語化テスト

--- taskmaster/src/App.tsx
+++ taskmaster/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ちゃおっす ぽい〜ん
+          Learn React
         </a>
       </header>
     </div>
